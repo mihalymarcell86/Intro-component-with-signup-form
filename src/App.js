4ba386caa2186df0c12aa62fabb536896bf3c9b8
Marcell Mihály <mihalymarcell86@gmail.com>
2022-10-13 22:53:13 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      Learn to code by watching others See how experienced developers solve
+      problems in real-time. Watching scripted tutorials is great, but
+      understanding how developers think is invaluable. Try it free 7 days then
+      $20/mo. thereafter First Name Last Name Email Address Password Claim your
+      free trial By clicking the button, you are agreeing to our Terms and
+      Services
+      <footer>
+        <p class="attribution">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a
+            href="https://www.github.com/mihalymarcell86"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Marcell Mihály
+          </a>
+          .
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </footer>
+    </>
   );
 }
 
